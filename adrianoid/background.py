c25d45a2cca98dbeb0d0481fce95c8a751c131bf
Vanzanji <adrjad88@gmail.com>
2022-04-25 16:07:33 +0200

    5 backgroundów

---
+++ adrianoid/background.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+import pygame.image
+
+
+class Background:
+    def __init__(self):
+        self.all_backgrounds = pygame.image.load(Path("grafiks", "backgrounds.png"))
+        self.background0 = pygame.Surface((223, 239))
+        self.background0.blit(self.all_backgrounds,(0,0),(370,14,223,239) )
+        self.background1 = pygame.Surface((223, 239))
+        self.background1.blit(self.all_backgrounds, (0, 0), (370, 246, 223, 239))
+        self.background2 = pygame.Surface((223, 239))
+        self.background2.blit(self.all_backgrounds, (0, 0), (370, 478, 223, 239))
+        self.background3 = pygame.Surface((223, 239))
+        self.background3.blit(self.all_backgrounds, (0, 0), (370, 710, 223, 239))
+        self.background4 = pygame.Surface((223, 239))
+        self.background4.blit(self.all_backgrounds, (0, 0), (370, 972, 223, 239))

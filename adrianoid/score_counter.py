5fb2d7a78e406a1bfb4b100d2e6daafd50377e42
Vanzanji <adrjad88@gmail.com>
2022-05-05 16:50:23 +0200

    score text

--- adrianoid/score_counter.py
+++ adrianoid/score_counter.py
@@ -4,9 +4,9 @@ import pygame
 class ScoreCounter:
     def __init__(self):
         self.current_score=0
-        self.my_font = pygame.font.SysFont('Comic Sans MS', 100)
-        self.text_surface = self.my_font.render(str(self.current_score), False, (255, 0, 0))
+        self.my_font = pygame.font.SysFont('Segoe Print', 75)
+        self.text_surface = self.my_font.render(str(self.current_score), False, (255, 234, 0))
 
     def add_score(self,score_number):
         self.current_score = self.current_score+score_number
-        self.text_surface = self.my_font.render(str(self.current_score), False, (255, 0, 0))
+        self.text_surface = self.my_font.render(str(self.current_score), False, (255, 234, 0))
